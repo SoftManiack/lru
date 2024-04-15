ae35e95f76ccb620e999eaa88c0656b840caa896
Suckhodoev Artem <fetelupercal@gmail.com>
2024-04-15 23:36:33 +0500

    insert end, append node

--- lits.go
+++ lits.go
@@ -21,16 +21,48 @@ func (l *DoubleLinkedList) NewDoubleLinkedList(size int) *DoubleLinkedList {
 	}
 }
 
-func (l *DoubleLinkedList) Add(value string) *Node {
+func (l *DoubleLinkedList) Add(value string) Node {
 
-	Node Node{}
+	var Node Node
 
 	if l.heade == nil {
-		l.heade = Node
 		Node.value = value
 		Node.previous = nil
 		Node.next = nil
+
+		l.heade = &Node
+		l.tail = &Node
+		l.size += 1
 	}
 
 	return Node
+}
+
+func (l *DoubleLinkedList) Remove() {
+
+}
+
+func (l *DoubleLinkedList) insertEnd(value string) {
+	if l.tail == nil {
+		node := Node{
+			value:    value,
+			previous: nil,
+			next:     nil,
+		}
+
+		l.tail = &node
+		l.heade = &node
+
+	} else {
+		node := Node{
+			value:    value,
+			previous: l.tail,
+			next:     nil,
+		}
+
+		l.tail.next = &node
+		l.tail = &node
+	}
+
+	l.size += 1
 }
